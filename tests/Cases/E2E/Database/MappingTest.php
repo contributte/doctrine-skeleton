0461cff11e5106326d03406bc020c3090a5e154e
Milan Felix Šulc <rkfelix@gmail.com>
2024-12-06 16:49:44 +0100

    Tests: fix mapping test

--- tests/Cases/E2E/Database/MappingTest.php
+++ tests/Cases/E2E/Database/MappingTest.php
@@ -3,8 +3,8 @@
 namespace Tests\Cases\E2E\Database;
 
 use App\Bootstrap;
-use App\Model\Database\EntityManagerDecorator;
 use Contributte\Tester\Toolkit;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Tools\SchemaValidator;
 use Tester\Assert;
 
@@ -13,12 +13,13 @@ require_once __DIR__ . '/../../../bootstrap.php';
 Toolkit::test(function (): void {
 	$container = Bootstrap::boot()->createContainer();
 
-	/** @var EntityManagerDecorator $em */
-	$em = $container->getByType(EntityManagerDecorator::class);
+	/** @var EntityManagerInterface $em */
+	$em = $container->getByType(EntityManagerInterface::class);
 
 	// Validation
 	$validator = new SchemaValidator($em);
 	$validations = $validator->validateMapping();
+
 	foreach ($validations as $fails) {
 		foreach ((array) $fails as $fail) {
 			Assert::fail($fail);
