1bf3d13d2a1aecca722b2b81815a1977aa8217af
Milan Felix Šulc <rkfelix@gmail.com>
2024-12-12 20:24:27 +0100

    Codesniffer: use 8.2 ruleset

--- app/UI/Home/HomePresenter.php
+++ app/UI/Home/HomePresenter.php
@@ -4,23 +4,32 @@ namespace App\UI\Home;
 
 use App\Domain\Database\User;
 use App\UI\BasePresenter;
-use Nette\DI\Attributes\Inject;
+use Doctrine\ORM\EntityManagerInterface;
 use Nette\Utils\Random;
-use Nettrine\ORM\ManagerRegistry;
+use Nettrine\ORM\ManagerProvider;
 
 class HomePresenter extends BasePresenter
 {
 
-	#[Inject]
-	public ManagerRegistry $managerRegistry;
+	private EntityManagerInterface $db1;
+
+	private EntityManagerInterface $db2;
+
+	public function __construct(ManagerProvider $managerProvider)
+	{
+		parent::__construct();
+
+		$this->db1 = $managerProvider->getManager('default');
+		$this->db2 = $managerProvider->getManager('second');
+	}
 
 	public function actionDefault(): void
 	{
 		// PostgreSQL
-		$users = $this->managerRegistry->getManager('default')->getRepository(User::class)->findAll();
+		$users = $this->db1->getRepository(User::class)->findAll();
 
 		// MariaDB
-		$users2 = $this->managerRegistry->getManager('second')->getRepository(User::class)->findAll();
+		$users2 = $this->db2->getRepository(User::class)->findAll();
 
 		$this->template->users = [...$users, ...$users2];
 	}
@@ -28,15 +37,15 @@ class HomePresenter extends BasePresenter
 	public function handleCreateUser(): void
 	{
 		$user = new User(Random::generate(20));
-		$this->managerRegistry->getManager('default')->persist($user);
-		$this->managerRegistry->getManager('default')->flush();
+		$this->db1->persist($user);
+		$this->db1->flush();
 		$this->flashMessage('Saved');
 		$this->redirect('this');
 	}
 
 	public function handleDeleteUsers(): void
 	{
-		$this->managerRegistry->getManager('default')->getRepository(User::class)
+		$this->db1->getRepository(User::class)
 			->createQueryBuilder('u')
 			->delete()
 			->getQuery()
